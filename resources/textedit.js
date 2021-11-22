9edab20cd460b9d55fa8934dcb1b94838a679242
rt3310 <btac3310@gmail.com>
2021-11-22 16:49:25 +0900

    텍스트 에디터 글자 정렬 및 un/redo 기능 추가, 이미지가 에디터를 넘어가는 현상 수정

--- resources/textedit.js
+++ resources/textedit.js
@@ -5,6 +5,14 @@ const btnUnderline = document.getElementById('tool-under');
 const btnStrike = document.getElementById('tool-cancel');
 const btnOrderedList = document.getElementById('tool-orderlist');
 const btnUnorderedList = document.getElementById('tool-unorderlist');
+const btnImage = document.getElementById('tool-image');
+const imageSelector = document.getElementById('image-select');
+const btnFull = document.getElementById('tool-full');
+const btnLeft = document.getElementById('tool-left');
+const btnCenter = document.getElementById('tool-center');
+const btnRight = document.getElementById('tool-right');
+const btnUndo = document.getElementById('tool-undo');
+const btnRedo = document.getElementById('tool-redo');
 
 btnBold.addEventListener('click', function() {
     setStyle('bold');
@@ -30,17 +38,29 @@ btnUnorderedList.addEventListener('click', function() {
     setStyle('insertUnorderedList');
 });
 
-function setStyle(style) {
-    document.execCommand(style);
-    focusEditor();
-}
+btnUndo.addEventListener('click', function() {
+    setStyle('undo');
+})
 
-function focusEditor() {
-    editor.focus({preventScroll: true});
-}
+btnRedo.addEventListener('click', function() {
+    setStyle('redo');
+});
 
-const btnImage = document.getElementById('tool-image');
-const imageSelector = document.getElementById('image-select');
+btnFull.addEventListener('click', function() {
+    setStyle('justifyFull')
+})
+
+btnLeft.addEventListener('click', function() {
+    setStyle('justifyLeft')
+})
+
+btnCenter.addEventListener('click', function() {
+    setStyle('justifyCenter')
+})
+
+btnRight.addEventListener('click', function() {
+    setStyle('justifyRight')
+})
 
 btnImage.addEventListener('click', function() {
     imageSelector.click();
@@ -53,6 +73,14 @@ imageSelector.addEventListener('change', function (e) {
     }
 });
 
+editor.addEventListener('keydown', function() {
+    checkStyle();
+});
+
+editor.addEventListener('mousedown', function() {
+    checkStyle();
+});
+
 function insertImageDate(file) {
     const reader = new FileReader();
     reader.addEventListener('load', function (e) {
@@ -60,4 +88,71 @@ function insertImageDate(file) {
         document.execCommand('insertImage', false, `${reader.result}`);
     });
     reader.readAsDataURL(file);
+}
+
+function focusEditor() {
+    editor.focus({preventScroll: true});
+}
+
+function setStyle(style) {
+    document.execCommand(style);
+    focusEditor();
+    checkStyle();
+}
+
+function checkStyle() {
+    if (isStyle('bold')) {
+        btnBold.classList.add('active');
+    } else {
+        btnBold.classList.remove('active');
+    }
+    if (isStyle('italic')) {
+        btnItalic.classList.add('active');
+    } else {
+        btnItalic.classList.remove('active');
+    }
+    if (isStyle('underline')) {
+        btnUnderline.classList.add('active');
+    } else {
+        btnUnderline.classList.remove('active');
+    }
+    if (isStyle('strikeThrough')) {
+        btnStrike.classList.add('active');
+    } else {
+        btnStrike.classList.remove('active');
+    }
+    if (isStyle('insertOrderedList')) {
+        btnOrderedList.classList.add('active');
+    } else {
+        btnOrderedList.classList.remove('active');
+    }
+    if (isStyle('insertUnorderedList')) {
+        btnUnorderedList.classList.add('active');
+    } else {
+        btnUnorderedList.classList.remove('active');
+    }
+    if (isStyle('justifyFull')) {
+        btnFull.classList.add('active');
+    } else {
+        btnFull.classList.remove('active');
+    }
+    if (isStyle('justifyLeft')) {
+        btnLeft.classList.add('active');
+    } else {
+        btnLeft.classList.remove('active');
+    }
+    if (isStyle('justifyCenter')) {
+        btnCenter.classList.add('active');
+    } else {
+        btnCenter.classList.remove('active');
+    }
+    if (isStyle('justifyRight')) {
+        btnRight.classList.add('active');
+    } else {
+        btnRight.classList.remove('active');
+    }
+}
+
+function isStyle(style) {
+    return document.queryCommandState(style);
 }
